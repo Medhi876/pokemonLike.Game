ea0c9cc6a5d894a178cdc40c1bd04858440ca3c0
Medhi876 <medhi876@gmail.com>
2022-07-11 10:56:26 +0200

    map modification

--- teste_projet.js
+++ teste_projet.js
@@ -1,17 +1,50 @@
-const canvas = document.querySelector('canvas'); //selectionne la balise canva en html, constente
-const context = canvas.getContext('2d'); //precise que le canvas sera 2d, constente
+const canvas = document.querySelector('canvas') //selectionne la balise canva en html, constente
+const context = canvas.getContext('2d') //precise que le canvas sera 2d, constente
 
-canvas.width = 1024; //largeur du canvas
-canvas.height = 576; //heuteur du canvas
+canvas.width = 1024 //largeur du canvas
+canvas.height = 576 //heuteur du canvas
 
-context.fillStyle = 'white'; //backgroud color du canvas
-context.fillRect(0, 0, canvas.width, canvas.height); //coordonné de remplissage du canvas
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 70) { //tile width de la map
+    collisionsMap.push(collisions.slice(i, 70 + i))
+}
 
-const image = new Image(); //constente "image" pour la map
-image.src = './img/Pokemon Style Game Map.png';  //source de la "new image" de type map
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor({ position }) {
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
 
-const playerImage = new Image (); //constente "playerImage" pour le personnage
-playerImage.src = './img/playerDown.png'; //source du personnage
+    draw() {
+        context.fillStyle = 'red'
+        context.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const boundaries = [];
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 2049)
+        boundaries.push(
+            new Boundary({
+                position: {
+                x: j * Boundary.width,
+                y: i * Boundary.height
+                }
+            })
+        )
+    })
+})
+
+const image = new Image() //constente "image" pour la map
+image.src = './img/Pokemon Style Game Map2.png'  //source de la "new image" de type map
+
+const playerImage = new Image () //constente "playerImage" pour le personnage
+playerImage.src = './img/playerDown.png' //source du personnage
 
 class Sprite {
     constructor({ position, velocity, image }) {
@@ -26,8 +59,8 @@ class Sprite {
 
 const background = new Sprite({ //spawn point du background
     position: { 
-        x: -415,
-        y: -520 
+        x: -160,
+        y: -290 
     }, 
     image: image
 });
@@ -50,6 +83,9 @@ const keys = {
 function animate() {
     window.requestAnimationFrame(animate)//appele de animate pour loop
     background.draw()//draw le background
+    boundaries.forEach(boundary => {
+        boundary.draw()
+    })
     context.drawImage( //draw la player image correctement
         playerImage,
         0,
@@ -62,57 +98,63 @@ function animate() {
         playerImage.height
     )
 
-    if(keys.z.pressed) {
+    if(keys.z.pressed && lastKey === 'z') {
         background.position.y = background.position.y + 3
     }
-    else if(keys.s.pressed) {
+    else if(keys.s.pressed && lastKey === 's') {
         background.position.y = background.position.y - 3
     }
-    else if(keys.q.pressed) {
+    else if(keys.q.pressed && lastKey === 'q') {
         background.position.x = background.position.x + 3
     }
-    else if(keys.d.pressed) {
+    else if(keys.d.pressed && lastKey === 'd') {
         background.position.x = background.position.x - 3
     }
-};
-animate(); //appele en loop d'une function pour l'animation
+}
+animate() //appele en loop d'une function pour l'animation
 
+let lastKey = ''
 window.addEventListener('keydown', (e) => {
     // console.log(e.key);
     switch (e.key) {
         case 'z':
             keys.z.pressed = true;
-            break;
+            lastKey = 'z'
+            break
         case 'q':
             keys.q.pressed = true;
-            break;
+            lastKey = 'q'
+            break
         case 's':
             keys.s.pressed = true;
-            break;
+            lastKey = 's'
+            break
         case 'd':
             keys.d.pressed = true;
-            break;
-    }; //definition du zqsd pour le mouvement du personnage 😉
-});
+            lastKey = 'd'
+            break
+    } //definition du zqsd pour le mouvement du personnage 😉 actif
+})
 
 window.addEventListener('keyup', (e) => {
-    // console.log(e.key);
+    // console.log(e.key)
     switch (e.key) {
         case 'z':
-            keys.z.pressed = false;
-            break;
+            keys.z.pressed = false
+            break
         case 'q':
-            keys.q.pressed = false;
-            break;
+            keys.q.pressed = false
+            break
         case 's':
-            keys.s.pressed = false;
-            break;
+            keys.s.pressed = false
+            break
         case 'd':
-            keys.d.pressed = false;
-            break;
-    }; //definition du zqsd pour le mouvement du personnage 😉
+            keys.d.pressed = false
+            break
+    } //definition du zqsd pour le mouvement du personnage 😉 inactif
+
     console.log(keys)
-});
+})
 
 
 
